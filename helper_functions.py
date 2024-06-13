58c809c75d96c0da78307274fe737bdad27fb595
gagoalaverdyan <gagoalaverdyan@gmail.com>
2024-06-14 01:09:39 +0400

    Implemented initial button handlers

--- helper_functions.py
+++ helper_functions.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 # Turns weather condition into an emoji
 def emojify(weather_state: str) -> str:
     match weather_state:
@@ -15,10 +18,34 @@ def emojify(weather_state: str) -> str:
             return "☁️"
         case _:
             return "💨"
+        
+# Turns wind degrees to a wind direction as a word 
+def get_wind_direction(degree: int) -> str:
+    if degree < 0 or degree > 360:
+        return "Invalid degree"
 
+    directions = [
+        "North", "North-Northeast", "Northeast", "East-Northeast",
+        "East", "East-Southeast", "Southeast", "South-Southeast",
+        "South", "South-Southwest", "Southwest", "West-Southwest",
+        "West", "West-Northwest", "Northwest", "North-Northwest"
+    ]
+
+    idx = round(degree / 22.5) % 16
+    return directions[idx]
+
+# Turns UTC shift in seconds into timezone
+def utc_to_timezone(shift_seconds):
+    hours = abs(shift_seconds // 3600)
+    minutes = abs((shift_seconds % 3600) // 60)
+    
+    sign = '+' if shift_seconds >= 0 else '-'
+    timezone_str = f"UTC{sign}{hours:02d}:{minutes:02d}"
+    
+    return timezone_str
 
 # Builds the current weather response
-def buld_weather_message(weather_data: dict) -> str:
+def build_weather_message(weather_data: dict) -> str:
     location = f"{weather_data["name"]}, {weather_data["sys"]["country"]}"
     desription = weather_data["weather"][0]["description"]
     temp = weather_data["main"]["temp"]
@@ -35,5 +62,53 @@ def buld_weather_message(weather_data: dict) -> str:
     message += f"📊 Pressure: {pressure} hPa\n"
     message += f"💧 Humidity: {humidity} %\n"
     message += f"🛣️ Visibility: {visibility} km\n"
+
+    return message
+
+# Builds the response for precipitation handler
+def build_precipitation_message(weather_data: dict) -> str:
+    message = ""
+
+    if "rain" in weather_data.keys():
+        message += "🌧️ Rain:\n"
+        for h, m in weather_data["rain"].items():
+            message += f"Last {h}: {m}mm\n"
+    if "snow" in weather_data.keys():
+        message += "❄️ Snow:\n"
+        for h, m in weather_data["snow"].items():
+            message += f"Last {h}: {m}mm\n"
+
+    if message:
+        return message
+    else:
+        return f"{emojify(weather_data["weather"][0]["main"])} No precipitation currently!\nEnjoy your day 😊"
+
+# Builds the response for wind handler
+def build_wind_message(weather_data: dict) -> str:
+    message = ""
+
+    speed = weather_data["wind"]["speed"]
+    deg = get_wind_direction(weather_data["wind"]["deg"])
+
+    message += f"🍃 Wind speed: {speed} m/s\n"
+    message += f"🧭 Wind direction: {deg}\n"
+
+    return message
+
+# Build the response for regional handler
+def build_regional_message(weather_data: dict) -> str:
+    message = ""
+
+    sunrise = datetime.fromtimestamp(weather_data["sys"]["sunrise"]).strftime("%H:%M")
+    sunset = datetime.fromtimestamp(weather_data["sys"]["sunset"]).strftime("%H:%M")
+    timezone = utc_to_timezone(weather_data["timezone"])
+    longitude = weather_data["coord"]["lon"]
+    latitude = weather_data["coord"]["lat"]
+
+    message += f"🌄 Sunrise: {sunrise}\n"
+    message += f"🌇 Sunset: {sunset}\n"
+    message += f"🌎 Timezone: {timezone}\n\n"
+    message += f"🧭 Longitude: {longitude}°\n"
+    message += f"🧭 Latitude: {latitude}°\n\n"
 
     return message
--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import telebot
 from environs import Env
 from telebot import types
 
-from helper_functions import buld_weather_message, emojify
+from helper_functions import (
+    build_precipitation_message,
+    build_regional_message,
+    build_weather_message,
+    build_wind_message,
+    emojify,
+)
 
 # Reading env
 env = Env()
@@ -11,6 +17,7 @@ env.read_env()
 
 # Bot initialization
 bot = telebot.TeleBot(env.str("BOT_API_KEY"))
+weather_data = dict()
 
 
 # Welcome message
@@ -26,8 +33,10 @@ def bot_start(message):
     )
 
 
+# Handling text messages as city queries
 @bot.message_handler(content_types=["text"])
 def city_handler(message):
+    global weather_data
     url = "https://api.openweathermap.org/data/2.5/weather"
     params = {
         "q": message.text.strip(),
@@ -41,14 +50,29 @@ def city_handler(message):
         current_state = weather_data["weather"][0]["main"]
 
         markup = types.InlineKeyboardMarkup()
-        btn1 = types.InlineKeyboardButton("🏞️ Additional", callback_data="additional")
-        btn2 = types.InlineKeyboardButton("🌧️ Precipation", callback_data="precipation")
-        btn3 = types.InlineKeyboardButton("💨 Wind", callback_data="wind")
-        btn4 = types.InlineKeyboardButton("🌤️ Forecast", callback_data="wind")
+        btn1 = types.InlineKeyboardButton("🌧️ Precipitation", callback_data="precipitation")
+        btn2 = types.InlineKeyboardButton("💨 Wind", callback_data="wind")
+        btn3 = types.InlineKeyboardButton("🏞️ Regional", callback_data="regional")
+        btn4 = types.InlineKeyboardButton("🌤️ Forecast", callback_data="forecast")
         markup.add(btn1, btn2, btn3, btn4)
 
         bot.send_message(message.chat.id, emojify(current_state))
-        bot.send_message(message.chat.id, buld_weather_message(weather_data), reply_markup=markup)
+        bot.send_message(message.chat.id, build_weather_message(weather_data), reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id, "🐧")
+        bot.send_message(message.chat.id, "Unable to find that city. Try again.")
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    if call.data == "precipitation":
+        bot.send_message(call.message.chat.id, build_precipitation_message(weather_data))
+    elif call.data == "wind":
+        bot.send_message(call.message.chat.id, build_wind_message(weather_data))
+    elif call.data == "regional":
+        bot.send_message(call.message.chat.id, build_regional_message(weather_data))
+    elif call.data == "forecast":
+        bot.send_message(call.message.chat.id, "Forecast info")
 
 
 bot.polling(none_stop=True)
